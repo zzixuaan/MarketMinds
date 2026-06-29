bc702d4b0d25bdc55e3ad66f17d03e1181da6428
kaih4n <tankaihan222@gmail.com>
2026-06-29 12:39:37 +0800

    fixed more links

--- my-app/src/services/tradeApi.ts
+++ my-app/src/services/tradeApi.ts
@@ -2,7 +2,7 @@ import { auth } from "../firebase-config";
 
 const BACKEND_URL =
   process.env.REACT_APP_BACKEND_URL ||
-  "http://localhost:8000";
+  "https://marketminds-i17q.onrender.com";
 
 export interface TradeEntryInput {
     symbol: string
