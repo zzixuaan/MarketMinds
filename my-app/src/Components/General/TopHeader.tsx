34a792327ce0af5d6f68d4cc79a80082738c99d3
kaih4n <tankaihan222@gmail.com>
2026-08-12 23:38:41 +0800

    logout and done testing

--- my-app/src/Components/General/TopHeader.tsx
+++ my-app/src/Components/General/TopHeader.tsx
@@ -2,23 +2,45 @@ import logo from "../../Pictures/Market_Minds_Logo_v3.png";
 import "../../cssComponents/TopHeader.css";
 import NavigationBar from "./NavigationBar";
 
+import { signOut } from "firebase/auth";
+import { auth } from "../../firebase-config";
+import { useNavigate } from "react-router-dom";
+
 
 function TopHeader() {
+    const navigate = useNavigate();
+
+    const handleLogout = async () => {
+        try {
+            await signOut(auth);
+            navigate("/");
+        } catch (error) {
+            console.error("Logout failed:", error);
+        }
+    };
+
     return (
         <div className="top-header">
             <div className="header-top">
                 <img src={logo} alt="Logo" className="logo"/>
 
-                <button className="notification-button">
-                    Notifications
-                </button>
+                <div className="header-actions">
+                    <button className="notification-button">
+                        Notifications
+                    </button>
+
+                    <button
+                        className="logout-button"
+                        onClick={handleLogout}
+                    >
+                        Logout
+                    </button>
+                </div>
             </div>
 
             <NavigationBar />
-
         </div>
-
-    )
+    );
 }
 
 export default TopHeader;
